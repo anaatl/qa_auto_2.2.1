226877c294833d2cb1a6a9923af27074f3949ecd
[AnastasiaAtl] <[anazta217@gmail.com]>
2025-04-01 18:19:02 +0300

    corrected code

--- calculations.js
+++ calculations.js
@@ -3,7 +3,7 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
+function decreaseBalance(x,y) {
 x = x - y;
 return x;
 }
@@ -11,7 +11,7 @@ return x;
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
+  // console.log(x);
 }
 
 function getRestAfterDivision(x, y) {
@@ -21,5 +21,5 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
+console.log(divideBalanceByAccounts(3000, 2));
 console.log(getRestAfterDivision(7000, 3));
